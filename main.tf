e08b4f4ac1521e1adf54df72381268e6894650a7
Gabriel Linero <gsuarez2@cas.org>
2025-10-13 14:30:07 -0700

    set up terraform

---
+++ main.tf
@@ -0,0 +1,64 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+locals {
+  marklogic_ports = [for port in range(8000, 8101) : port]
+}
+
+variable "admin_password" {
+  type        = string
+  description = "MarkLogic admin password"
+  sensitive   = true
+}
+
+resource "docker_image" "marklogic" {
+  name         = "progressofficial/marklogic-db:latest"
+  keep_locally = true
+}
+
+resource "docker_volume" "marklogic" {
+  name = "ml_vol_1"
+}
+
+resource "docker_container" "marklogic" {
+  name  = "marklogic-local"
+  image = docker_image.marklogic.image_id
+
+  env = [
+    "MARKLOGIC_ADMIN_USER=admin",
+    "MARKLOGIC_ADMIN_PASSWORD=${var.admin_password}",
+    "MARKLOGIC_INIT=true"
+  ]
+
+  dynamic "ports" {
+    for_each = local.marklogic_ports
+    content {
+      internal = ports.value
+      external = ports.value
+    }
+  }
+
+  restart = "unless-stopped"
+
+  volumes {
+    volume_name    = docker_volume.marklogic.name
+    container_path = "/var/opt/MarkLogic"
+  }
+
+  healthcheck {
+    test     = ["CMD", "curl", "-f", "http://localhost:8001/admin/v1/timestamp"]
+    interval = "30s"
+    timeout  = "5s"
+    retries  = 10
+  }
+}
